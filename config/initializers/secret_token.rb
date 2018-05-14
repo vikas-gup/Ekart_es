9bbb5a3216eee0879297fc715b66d39da83b7336
vikas.gupta <vikas.gupta@traveltriangle.com>
2018-05-14 10:43:05 +0530

    initial commit rails set with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ekart::Application.config.secret_token = '122b9c4b8da8462f02f579e239634c5e1d1ab73f05af530635e17748238c1a23bcf6f1f9d81b589a1f351424838f09d1ff07133b5379cf7d5d36e8530ba23d2c'
